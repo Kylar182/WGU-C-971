592386ee340e880b99afedab60292d71158a0e01
Andrew J. Beeman <ajbeeman@kfsoftwaresolutions.com>
2022-04-01 04:53:11 -0500

    Course Page

---
+++ C971/C971/ViewModels/CourseVM.cs
@@ -0,0 +1,15 @@
+﻿using C971.Models.DatabaseModels;
+using C971.Services;
+using Xamarin.Forms;
+
+namespace C971.ViewModels
+{
+  public class CourseVM : BaseCrudPageVM<Course>
+  {
+    public CourseVM()
+    {
+      Title = "Courses";
+      Service = DependencyService.Get<ICourseService>();
+    }
+  }
+}
---
+++ C971/C971/Views/CoursesPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using C971.Models.DatabaseModels;
+using C971.ViewModels;
+
+namespace C971.Views
+{
+  public partial class CoursesPage : BaseCrudPage<CourseVM, Course>
+  {
+    public CoursesPage()
+    {
+      InitializeComponent();
+
+      BindingContext = _viewModel = new CourseVM();
+    }
+  }
+}
